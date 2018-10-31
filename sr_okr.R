0c218c24140f72a62dda1c944c1dabb522ff9f7b
olexster <olexster@gmail.com>
2018-10-31 12:33:53 +0100

    początkowe notatki

---
+++ sr_okr.R
@@ -0,0 +1,13 @@
+sr_okr<-function(xy){
+  xy<-as.matrix(xy)
+  a<-cbind(xy*2,1) #-2ax-2bY+c
+  A<-t(a)%*%a
+  B<-t(a)%*%rowSums(xy^2) #-2ax-2bY+c=x^2+y^2
+  # covA<-cov(A)^0.5 #macierz comawiancji mx[i]=(cov(A)[i,i])^0.5
+  X<-solve(A,B)
+  print(X)
+  #   print(A)
+  #   cat('\n')
+  print(cov(A)^0.5)
+  return(X[1:2])
+}
