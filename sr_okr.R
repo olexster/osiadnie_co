a9225bc90fe3b07fdb0331a39cdb0736977446ff
olexster <olexster@gmail.com>
2018-11-05 11:47:30 +0100

    funkcja dla 3ch punktów

--- sr_okr.R
+++ sr_okr.R
@@ -1,11 +1,12 @@
 sr_okr<-function(xy){
   xy<-as.matrix(xy)
   a<-cbind(xy*2,1) #-2ax-2bY+c
-  A<-t(a)%*%a
-  B<-t(a)%*%rowSums(xy^2) #-2ax-2bY+c=x^2+y^2
+  # A<-t(a)%*%a
+  # B<-t(a)%*%rowSums(xy^2) #-2ax-2bY+c=x^2+y^2
+  b<-rowSums(xy^2) #-2ax-2bY+c=x^2+y^2
   # covA<-cov(A)^0.5 #macierz comawiancji mx[i]=(cov(A)[i,i])^0.5
-  X<-solve(A,B)
-  print(X)
+  X<-solve(a,b)
+  print(x)
   #   print(A)
   #   cat('\n')
   print(cov(A)^0.5)
