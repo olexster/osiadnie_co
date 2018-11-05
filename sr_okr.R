8abfcf08dcc369eb5fba42933b90e70dc06c23a6
olexster <olexster@gmail.com>
2018-11-05 12:00:08 +0100

    i znów bugi były

--- sr_okr.R
+++ sr_okr.R
@@ -5,7 +5,7 @@ sr_okr<-function(xy){
   # B<-t(a)%*%rowSums(xy^2) #-2ax-2bY+c=x^2+y^2
   b<-rowSums(xy^2) #-2ax-2bY+c=x^2+y^2
   # covA<-cov(A)^0.5 #macierz comawiancji mx[i]=(cov(A)[i,i])^0.5
-  X<-solve(a,b)
+  x<-solve(a,b)
   print(x)
   #   print(A)
   #   cat('\n')
