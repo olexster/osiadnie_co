0c0add40a476f0923ac21814006a151ae4dcd184
olexster <olexster@gmail.com>
2018-11-09 14:59:55 +0100

    poprawa błedów

--- mian.R
+++ mian.R
@@ -1,8 +1,9 @@
 s1 <- read.table("~/roboczy/osiadanie nogi co2/s1.txt", 
                  quote="\"", 
+                 dec = ".",
                  comment.char="",
 col.names = c("Nr:","X","Y","H")
 )
 
-s1$poziom<-cut(s1$V4,2,labels = c("d","g"))
+s1$poziom<-cut(s1$H,2,labels = c("d","g"))
 sr_okr()
