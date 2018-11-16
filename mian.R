511a0f83d03225e28149cbcfd4bced66bf14d981
olexster <olexster@gmail.com>
2018-11-16 06:18:29 +0100

    filtrowanie punktów gurnych i dolnych oraz oblicznie odchylenia od pionu

--- mian.R
+++ mian.R
@@ -6,4 +6,5 @@ col.names = c("Nr:","X","Y","H")
 )
 
 s1$poziom<-cut(s1$H,2,labels = c("d","g"))
-sr_okr()
+# pochylenie slupa 1
+sr_okr(as.matrix(s1[which(s1$poziom=="g"),-c(1,4,5)]))-sr_okr(as.matrix(s1[which(s1$poziom=="d"),-c(1,4,5)]))
